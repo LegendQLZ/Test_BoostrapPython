0bf4a4b03a81d6222777c7528ff4a48b1e806fec
Theo Gence <theo.gence@epitech.eu>
2021-11-18 06:29:35 -0500

    boostrap

---
+++ Boostrap_Python/variafunc.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+fruit = "banana"
+quantity = 3
+pie_crust = "empty"
+isOvenOn = False
+
+print(f"You have {quantity} {fruit} and the pie crust is {pie_crust}, is the oven on ? {isOvenOn}")
